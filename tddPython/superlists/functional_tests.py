16c09481997db2fee1afa7aaabb3b1d7d12eb870
Robert Stanton <rstanton@illinois.edu>
2015-12-21 11:13:34 -0600

    Adding unittest functionality to existing code

--- tddPython/superlists/functional_tests.py
+++ tddPython/superlists/functional_tests.py
@@ -1,5 +1,36 @@
 from selenium import webdriver
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+import unittest
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+  def setUp(self):
+    self.browser = webdriver.Firefox()
+    self.browser = implicitly_wait(3)
+
+  def tearDown(self):
+    self.browser.quit()
+
+  def test_can_start_a_list_and_retrieve_it_later(self):
+    #As a user, I want to be able to go to the expected URL to access
+    #the online to-do application
+    self.browser.get('http://localhost:8000')
+
+    #User should note the appropriate title and header in the web-page
+    self.assertIn('To-do', self.browser.title)
+    self.fail("Finish the test!")
+
+    #User should be invited to create a to-do item immediately
+
+    #User enters a to-do item into a text box
+
+    #User confirms entry, the page updates, and page lists the user entry
+
+    #User can add another item to the list if desired
+
+    #If user adds another item, page updates again and shows two items in list
+
+    #User's list should be saved and available via a URI
+
+    #User closes the application upon seeing the list is saved/available
+
+if __name__ == '__main__':
+  unittest.main(warnings='ignore')
