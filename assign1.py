0295fd7c6c7de4e011fde5b5626f8a41ef25d392
Robert Stanton <rstanton@illinois.edu>
2016-01-27 10:21:12 -0600

    Added error checking and function for returning the maximum and minimum of the list

--- assign1.py
+++ assign1.py
@@ -2,11 +2,30 @@ total = 0
 myList = []
 while True:
   myIn = input("Input a digit:  ")
-  if myIn == "done":
-    print("# of elements in list: ", len(myList), "\n")
-    print("average of list elements: ", float(total)/len(myList), "\n")
-    print("total of list elements: ", total, '\n')
-    break
-  else:
-    myList.append(int(myIn))
-    total += int(myIn) 
+  try:
+    if myIn == "done":
+      print("# of elements in list: ", len(myList), "\n")
+      print("average of list elements: ", float(total)/len(myList), "\n")
+      print("total of list elements: ", total, '\n')
+      break
+    else:
+      myList.append(int(myIn))
+      total += int(myIn)
+  except:
+    print("Bad data")
+
+
+total=0
+myList = []
+while True:
+  myIn = input("Input a digit: ")
+  try:
+    if myIn == "done":
+      myList.sort()
+      print('Min value in list is: ', myList[0])
+      print('Max value in list is: ', myList[len(myList)-1])
+      break
+    else:
+      myList.append(int(myIn))
+  except:
+    print('Invalid entry')
