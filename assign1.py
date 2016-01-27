1519be05e4b8b0d151e3e974b44c5ea7b86c0530
Robert Stanton <rstanton@illinois.edu>
2016-01-27 09:54:11 -0600

    Wrote code to ask for user input and handle it appropriately

--- assign1.py
+++ assign1.py
@@ -1,1 +1,12 @@
-print "hello world"
+total = 0
+myList = []
+while True:
+  myIn = input("Input a digit:  ")
+  if myIn == "done":
+    print("# of elements in list: ", len(myList), "\n")
+    print("average of list elements: ", float(total)/len(myList), "\n")
+    print("total of list elements: ", total, '\n')
+    break
+  else:
+    myList.append(int(myIn))
+    total += int(myIn) 
