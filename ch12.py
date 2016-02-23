03c48a156808add23b9c56a7519134d5dbbe1a49
Robert Stanton <rstanton@illinois.edu>
2016-02-23 11:14:54 -0600

    Add working socket code

--- ch12.py
+++ ch12.py
@@ -1,15 +1,21 @@
 import socket
+
+#create an INET, streaming socket
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#connect to the web server on port 80, the normal http port
 mysock.connect(('www.py4inf.com', 80))
 msg='GET http://www.py4inf.com/code/romeo.txt HTTP/1.0\n\n'
 totalsent=0
-#mysock.send(msg[totalsent:])
+#mysock.send(b"msg")
+mysock.send(b'GET http://www.py4inf.com/code/romeo.txt HTTP/1.0\n\n')
 
 while True:
+  print("in while loop")
   data = mysock.recv(512)
   if ( len(data) < 1 ) :
     break
-  print(data)
+  print("data is ", data)
 
 mysock.close()
 
